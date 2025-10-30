90c649cedc3803451ce02ab9af2d7268923ceb18
Orr Morag <orr.morag@post.runi.ac.il>
2025-10-30 09:19:58 +0200

    Complete Bill3 program

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,12 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name1 = args[0];  // Ron
+		String name2 = args[1];  // Lisa
+		String name3 = args[2];  // Dan
+		int totalBill = Integer.parseInt(args[3]);  // 100
+		double eachPays = Math.ceil(totalBill/3.0);   // 34
+	    System.out.println("Dear " + name3 + ", " + name2 + ", " + 
+						   "and " + name1 + ": pay " + eachPays + " Shekels each.");
 	}
 }
