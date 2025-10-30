b321634d36f9f4f0e7e87896648e80960cb2b4ed
Orr Morag <orr.morag@post.runi.ac.il>
2025-10-30 23:36:19 +0200

    Bill3 program completed

--- Bill3.java
+++ Bill3.java
@@ -4,7 +4,7 @@ public class Bill3 {
 		String name1 = args[0];  // Ron
 		String name2 = args[1];  // Lisa
 		String name3 = args[2];  // Dan
-		int totalBill = Integer.parseInt(args[3]);  // 100
+		int totalBill = Integer.parseInt(args[3]);    // 100
 		double eachPays = Math.ceil(totalBill/3.0);   // 34
 	    System.out.println("Dear " + name3 + ", " + name2 + ", " + 
 						   "and " + name1 + ": pay " + eachPays + " Shekels each.");
