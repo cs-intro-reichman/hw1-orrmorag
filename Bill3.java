7763307ae1bdc3eda7d15b62d246aff10b1ffe5b
Orr <orrmorag@post.runi.ac.il>
2025-10-30 15:50:30 +0200

    Fix FVCalc.java to correctly calculate future value

--- Bill3.java
+++ Bill3.java
@@ -1,7 +1,6 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
 		String name1 = args[0];  // Ron
 		String name2 = args[1];  // Lisa
 		String name3 = args[2];  // Dan
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]); // 1000
+		double rate = Double.parseDouble(args[1]); // 10
+		int n = Integer.parseInt(args[2]); // 2
+		int futureValue = (int) (currentValue * Math.pow((1 + rate/100), n));
+		System.out.println("After " + n + " years, $" + currentValue +
+						   " saved at " + rate + "% will yield $" + futureValue);
 	}
 }
